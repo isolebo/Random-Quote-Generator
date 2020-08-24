48b1fefce49f7cee6a3f4b69c62cebf9eabd8619
Isiaq Solebo <i.solebo32@gmail.com>
2020-08-24 15:37:59 -0500

    Created an array of objects to store the quotes.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Random Quote Generator
-s
+
  
 
--- js/script.js
+++ js/script.js
@@ -10,7 +10,28 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  { quote : "The greatest glory lies not in  never falling, but in rising every time we fall",
+    source: "Nelson Mandel"
+  },
+  { quote : "The only impossible journey is the one you never begin",
+    source: "Tony Robbins"
+  },
+  { quote : "Whoever is happy will make others happy too",
+    source: "Anne Frank"
+  },
+  { quote : "A man's true wealth is the good he does in the world",
+    source: "Khalil Gibran"
+  },
+  { quote : "Life moves pretty fast. if you don't and look around once ina while, you could miss it",
+    source: "Ferris Bueller",
+    citation: "Ferris Bueller's Day Off"
+  },
+  { quote : "What we do in life echoes in eternity",
+    source: "Maximus,Gladiator",
+    year: 2000
+  }
+]
 
 
 /***
@@ -31,3 +52,4 @@ project 1 - A Random Quote Generator
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+console.log('test');
