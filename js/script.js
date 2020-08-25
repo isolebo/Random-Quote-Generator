6c81ff4e1b85c9ba7cbf2a7c0d90d6160b3c6005
Isiaq Solebo <i.solebo32@gmail.com>
2020-08-24 20:44:22 -0500

    Revert "The getRandomQuote was created"
    This reverts commit 749675ddd80893de8daa6248891a7ce650e76b58.

--- js/script.js
+++ js/script.js
@@ -33,17 +33,12 @@ const quotes = [
   }
 ]
 
+
 /***
  * `getRandomQuote` function
 ***/
-// function getRandomQuote(){
-//   
-    function getRandomQuote(){//
-    
-      const randomQuote = Math.floor(Math.random() * (quotes.length)); //generates a random  number of quote between the first 
-                                                                       //and last element
-      return quotes[randomQuote]; // returns the quotes in random as an object
-    }
+
+
 
 /***
  * `printQuote` function
@@ -56,4 +51,5 @@ const quotes = [
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
+console.log('test');
