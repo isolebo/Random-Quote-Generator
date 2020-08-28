b59c09c7525fceff988613f1aa683cf0ff33c1e0
Isiaq Solebo <i.solebo32@gmail.com>
2020-08-27 23:59:35 -0500

    Created the getRandomColor function, and made some adjustment to the code to fit project specification

--- js/script.js
+++ js/script.js
@@ -7,12 +7,18 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-const quotes = [
+let quotes = [
   { quote : "The greatest glory lies not in  never falling, but in rising every time we fall",
     source: "Nelson Mandela"
   },
   { quote : "The only impossible journey is the one you never begin",
     source: "Tony Robbins"
+  },
+  { quote: "They destroy the man I was, but then I was reborn, and this time I came out breathing fire",
+    source: "Akecheta",
+    citation: "Westworld: Season 2",
+    year: "2016"
+
   },
   { quote : "Whoever is happy will make others happy too",
     source: "Anne Frank"
@@ -28,6 +34,12 @@ const quotes = [
   { quote : "What we do in life echoes in eternity",
     source: "Maximus, Gladiator",
     year: "2000"
+  },
+  { quote: "When you play the game of thrones, you win or die",
+    source: "Cersei Lannister",
+    citation: "Game of Thrones: Season 1; Episode 1",
+    year: "2011"
+
   }
 ]
 
@@ -49,30 +61,39 @@ const quotes = [
  * `printQuote` function
 ***/
     function printQuote(){ //this function pints the quotes according to certain conditions
-        let printQuote = getRandomQuote();//it sets the variable to call the getRandomQuote function
-        let html = " ";//sets the html variAble to empty strings initially
-        var message = "<p class = quote "> + quotes.quote + "</p>" + "<p class ='source'" + quotes.source + "</p";
-        document.getElementById('quote-box').innerHTML = message;
-        html += '<p class = "quote">' + printQuote.quote + '</p>';//the quote printout
-        html += '<p class = "source">' + printQuote.source + '</p>'//the source printout
 
-        if("citation" in printQuote ){//checks to see if  the object has a year, and it prints to the page
-          html += '<span class = "citation" >' + printQuote.citation  + '</p>';
+        let newQuote = getRandomQuote();//calls the getRandomQuote function
+        let html = " ";//sets the html variAble to empty strings initially
+        html += '<p class = "quote">' + newQuote.quote + '</p>';//the quote printout
+        html += '<p class = "source">' + newQuote.source ;//the source printout
+
+        if("citation" in newQuote ){//checks to see if  the object has a year, and it prints to the page
+          html += '<span class = "citation" >' + newQuote.citation  ;
       }
-           if("year" in printQuote ){ //checks to see if  the object has a year, and it prints to the page
-           html += '<span class = "year" >' + printQuote.year+ '</span>' + '</p>';
+           if("year" in newQuote ){ //checks to see if  the object has a year, and it prints to the page
+           html += '<span class = "year" >' + newQuote.year + '</span>'+ '</p>';
       }
       
             document.getElementById("quote-box").innerHTML=html;
     }
 
+
+    function getRandomColor() {//function generates random color
+      var color1 = Math.floor(Math.random() * 256);
+      var color2 = Math.floor(Math.random() * 256);
+      var color3 = Math.floor(Math.random() * 256);
+      var randomColor = "rgb(" + color1 + "," + color2 + "," + color3 + ")";
+      console.log(randomColor);
     
+        document.body.style.background = randomColor;//applies random color to background
+      }
+  
 
     function changeQuote(){ //function to refrsh the quotes
           document.location.reload();
 
     }
-
+    getRandomColor();//calls the function
     printQuote();//calls the function and outputs the quotes 
     console.log(printQuote());
     setInterval(changeQuote, 10000);//setInterval passes a function as a parameter to refresh the quotes every 10sec
@@ -81,5 +102,5 @@ const quotes = [
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-//when the button ic clicked a different quote pops up
+//when the button is clicked a different quote pops up
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
